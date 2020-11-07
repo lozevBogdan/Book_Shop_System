2b6c9afc9044d08373d26859116683d4d71c939f
lozev <lozev.bogdan@abv.bg>
2020-11-07 10:31:08 +0200

    Completed

--- src/main/java/com/softwareexample/springintroexercise/Controllers/AppController.java
+++ src/main/java/com/softwareexample/springintroexercise/Controllers/AppController.java
@@ -4,6 +4,7 @@ import com.softwareexample.springintroexercise.Entities.Book;
 import com.softwareexample.springintroexercise.Servises.AuthorService;
 import com.softwareexample.springintroexercise.Servises.BookService;
 import com.softwareexample.springintroexercise.Servises.CategoryService;
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,8 @@ import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Scanner;
+
 
 @Controller
 public class AppController implements CommandLineRunner {
@@ -33,33 +36,45 @@ public class AppController implements CommandLineRunner {
         this.authorService.seedAuthors();
         this.bookService.seedBooks();
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.printf("Welcome to My Homework: 'Spring data intro',%n" +
+                "please enter which query from Book System you want to execute or 'STOP' for exit:%n");
+        String input = scanner.nextLine();
 
-        //Ex1
-//        List<Book> bookList = this.bookService.getAllBooksAfter2000();
-//        bookList.forEach(e->{
-//            System.out.println(e.getTitle());
-//        });
+        while (!input.equalsIgnoreCase("STOP")){
+            int n = Integer.parseInt(input);
 
-        //Ex2
-//        this.authorService.authorsWithBookBefore1990()
-//                .forEach(a-> System.out.printf("%s %s%n",
-//                        a.getFirstName(),a.getLastName()));
+            switch (n){
+                case 1:
+                    List<Book> bookList = this.bookService.getAllBooksAfter2000();
+                     bookList.forEach(e-> {
+                         System.out.println(e.getTitle());
+                     });
+                    break;
 
-
-        //Ex3
-//        this.authorService.listOfAuthorByCountOfBooks()
-//                .forEach(e->{
-//                    System.out.printf("%s %s %d%n",
-//                            e.getFirstName(),
-//                            e.getLastName(),
-//                            e.getBooks().size());
-//                });
-
-                // EX4
-//        this.bookService.getAllBookByGeorgePowell()
-//                .forEach(b-> System.out.printf("%s %s %d%n",b.getTitle(),
-//                        b.getReleaseDate().toString(), b.getCopies()));
-
+                case 2:
+                    this.authorService.authorsWithBookBefore1990()
+                .forEach(a-> System.out.printf("%s %s%n",
+                        a.getFirstName(),a.getLastName()));
+                    break;
+                case 3:
+        this.authorService.listOfAuthorByCountOfBooks()
+                .forEach(e->{
+                    System.out.printf("%s %s %d%n",
+                            e.getFirstName(),
+                            e.getLastName(),
+                            e.getBooks().size());
+                });
+                    break;
+                case 4:
+        this.bookService.getAllBookByGeorgePowell()
+                .forEach(b-> System.out.printf("%s %s %d%n",b.getTitle(),
+                        b.getReleaseDate().toString(), b.getCopies()));
+                    break;
+            }
+            System.out.println("please enter which query from Book System you want to execute or 'STOP' for exit:");
+            input =scanner.nextLine();
+        }
     }
 }
